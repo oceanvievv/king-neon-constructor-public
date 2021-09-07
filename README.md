1764bd920998ced3efb4debbd7e0b9a938e99b47
Matvey Lifschits <6273834-beliyzlodey@users.noreply.gitlab.com>
2021-09-07 16:30:58 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# king-neon-constructor-public" 
---
+++ precache-manifest.51661f648c7798c9150c063b47115717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827a08619150c4aaf9c2",
+    "url": "/css/app.29c29790.css"
+  },
+  {
+    "revision": "4dd08e37b50e24e600e4",
+    "url": "/css/chunk-vendors.e1da2453.css"
+  },
+  {
+    "revision": "a667cd5f3a896cb9713236314192eb3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "827a08619150c4aaf9c2",
+    "url": "/js/app.402acecd.js"
+  },
+  {
+    "revision": "4dd08e37b50e24e600e4",
+    "url": "/js/chunk-vendors.e85f3dd5.js"
+  },
+  {
+    "revision": "eae7daba9ac3ab5200df217e48905956",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
